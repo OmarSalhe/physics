eab530ebb53bc778349683ff10710f3e52384506
Omar <osalhe12@gmail.com>
2024-09-28 08:58:08 -0400

    added remaining functions

--- PhysicsObject.cpp
+++ PhysicsObject.cpp
@@ -1,17 +1,68 @@
 #include "PhysicsObject.h"
 #include "Vector.h"
+#include <cmath>
 
-PhysicsObject::PhysicsObject(double t, double m, Vector pos, Vector vel, Vector acc, Vector net): mass(mass), time(t), position(pos), velocity(vel), acceleration(acc), netForce(net){}
-
+PhysicsObject::PhysicsObject(double m, Vector pos, Vector vel, Vector acc, Vector net): mass(mass), position(pos), velocity(vel), acceleration(acc), netForce(net){}
 PhysicsObject::~PhysicsObject(){}
 
+Vector PhysicsObject::getPosition() const{
+    return position;
+}
+Vector PhysicsObject::getVelocity() const{
+    return velocity;
+}
+Vector PhysicsObject::getAcceleration() const{
+    return acceleration;
+}
+
 void PhysicsObject::updatePosition(){
+updateVelocity();
+
+// v(x, t) = dx/dt = v + x''(t)*t
+double h = .001;
+
+double ax = acceleration.xComponent;
+double ay = acceleration.yComponent;
+double az = acceleration.zComponent;
+
+double k1x = velocity.xComponent;
+double k2x = k1x + ax * (h/2);
+double k3x = k2x + ax * (h/2);
+double k4x = k3x + ax * h;
+
+double k1y = velocity.yComponent;
+double k2y = k1y + ay * (h/2);
+double k3y = k2y + ay * (h/2);
+double k4y = k3y + ay * h;
+
+double k1z = velocity.zComponent;
+double k2z = k1z + az * (h/2);
+double k3z = k2z + az * (h/2);
+double k4z = k3z + az * h;
+
+position.xComponent += (h/6) * (k1x + 2*k2x + 2*k3x + k4x);
+position.yComponent += (h/6) * (k1y + 2*k2y + 2*k3y + k4y);
+position.zComponent += (h/6) * (k1z + 2*k2z + 2*k3z + k4z);
+}
+
+double PhysicsObject::distanceTo(PhysicsObject& other) const{
+    return (position - other.getPosition()).magnitude();
+}
+
+void PhysicsObject::updateVelocity(){
+    updateAcceleration();
     double h = .001;
-    double k1x, k1y, k1z, k2x, k2y, k2z, k3x, k3y, k3z, k4x, k4y, k4z;
-    double x = position.getX(), y = position.getY(), z = position.getZ();
+    
+    velocity += (acceleration) * h;
+}
 
-    // from the dx/dt = v:
-    double k1x = 0;
+void PhysicsObject::updateAcceleration(){
+    clearForce();
+    acceleration.yComponent = 9.8;
+}
 
-
+void PhysicsObject::clearForce(){
+    netForce.xComponent = 0;
+    netForce.yComponent = 0;
+    netForce.zComponent = 0;
 }
--- PhysicsObject.h
+++ PhysicsObject.h
@@ -4,12 +4,14 @@
 
 class PhysicsObject {
     public:
-        PhysicsObject(double mass, double time = 0, Vector position, Vector velocity, Vector acceleration, Vector netForce);
+        PhysicsObject(double mass, Vector position, Vector velocity, Vector acceleration, Vector netForce);
         ~PhysicsObject();
 
         Vector getPosition() const;
         Vector getVelocity() const;
         Vector getAcceleration() const;
+
+        double distanceTo(PhysicsObject& other) const;
 
         void applyForce(Vector force);
         void updatePosition();
@@ -17,15 +19,12 @@ class PhysicsObject {
     private:
         double mass, time;
         Vector position, velocity, acceleration, netForce;
-
         void clearForce();
-        double calculateVelocity();
-        double calculateAcceleration();
+        void updateAcceleration();
         Vector calculateMomentum() const;
         double calculateKineticEnergy() const;
         Vector velocityFromKineticEnergy(double KE);
         void updateVelocity();
 };
-
 #endif
 
