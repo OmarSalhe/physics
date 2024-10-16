c12bb7f92d53e3216ffb7d9503c83d2e769e1d8c
Omar <osalhe12@gmail.com>
2024-10-16 18:45:46 -0400

    changed back to class operations

--- PhysicsObject.cpp
+++ PhysicsObject.cpp
@@ -21,28 +21,28 @@ updateVelocity();
 // v(x, t) = dx/dt = v + x''(t)*t
 double h = .001;
 
-double ax = acceleration.xComponent;
-double ay = acceleration.yComponent;
-double az = acceleration.zComponent;
+double ax = acceleration.getX();
+double ay = acceleration.getY();
+double az = acceleration.getZ();
 
-double k1x = velocity.xComponent;
+double k1x = velocity.getX();
 double k2x = k1x + ax * (h/2);
 double k3x = k2x + ax * (h/2);
 double k4x = k3x + ax * h;
 
-double k1y = velocity.yComponent;
+double k1y = velocity.getX();
 double k2y = k1y + ay * (h/2);
 double k3y = k2y + ay * (h/2);
 double k4y = k3y + ay * h;
 
-double k1z = velocity.zComponent;
+double k1z = velocity.getZ();
 double k2z = k1z + az * (h/2);
 double k3z = k2z + az * (h/2);
 double k4z = k3z + az * h;
 
-position.xComponent += (h/6) * (k1x + 2*k2x + 2*k3x + k4x);
-position.yComponent += (h/6) * (k1y + 2*k2y + 2*k3y + k4y);
-position.zComponent += (h/6) * (k1z + 2*k2z + 2*k3z + k4z);
+position.setX(position.getX() + (h/6) * (k1x + 2*k2x + 2*k3x + k4x));
+position.setY(position.getY() + (h/6) * (k1y + 2*k2y + 2*k3y + k4y));
+position.setY(position.getZ() + (h/6) * (k1z + 2*k2z + 2*k3z + k4z));
 }
 
 double PhysicsObject::distanceTo(PhysicsObject& other) const{
@@ -58,11 +58,10 @@ void PhysicsObject::updateVelocity(){
 
 void PhysicsObject::updateAcceleration(){
     clearForce();
-    acceleration.yComponent = 9.8;
 }
 
 void PhysicsObject::clearForce(){
-    netForce.xComponent = 0;
-    netForce.yComponent = 0;
-    netForce.zComponent = 0;
+    netForce.setX(0);
+    netForce.setY(0);
+    netForce.setZ(0);
 }
